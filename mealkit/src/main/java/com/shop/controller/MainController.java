9411a409972de2682c74a33451f6ab77d77c86f7
Knitty-Kim <kimkitae1208@gmail.com>
2023-01-12 10:56:56 +0900

    2023.01.12 김기태 헤더/푸터 추가

--- mealkit/src/main/java/com/shop/controller/MainController.java
+++ mealkit/src/main/java/com/shop/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.shop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +11,20 @@ public class MainController {
 	public String main() {
 		return "main";
 	}
+	
+	//상품 전체 리스트
+	@RequestMapping("/shop")
+	public String shop(Model model) {
+		
+		return "shop";
+	}
+	
+	//상품 상세 보기
+	@RequestMapping("/product_single")
+	public String product_single(Model model) {
+		
+		return "product_single";
+	}
+	
+	
 }
